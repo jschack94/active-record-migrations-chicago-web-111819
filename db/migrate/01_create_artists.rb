d92799ff33b3218386e144d3a5a7dfcd78e930bc
Jonathan Schack <jschack94@gmail.com>
2019-11-30 05:15:17 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -1,7 +1,10 @@
-class CreateArtists < ActiveRecord::Migration[5.2]
-  def up
-  end
-
-  def down
+class CreateArtists < ActiveRecord::Migration[4.2]
+  def change
+    create_table :artists do |t|
+      t.string :name
+      t.string :genre
+      t.integer :age
+      t.string :hometown
+    end
   end
 end
